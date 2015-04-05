2eb4e03eb091e4f3f79b48b1315452debddab456
Harshit <hpj1992@gmail.com>
2015-04-05 15:27:13 -0700

    try2

--- src/main/java/com/cmpe275/game/HomeController.java
+++ src/main/java/com/cmpe275/game/HomeController.java
@@ -8,11 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 /**
- * Handles requests for the application home page. Kunal
+ * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
@@ -35,5 +37,6 @@ public class HomeController {
 		
 		return "home";
 	}
+	
 	
 }
